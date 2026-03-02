4bd5ef7362bc7ba6cfb45653b7f62c48649f91f5
Parker Arnett <362581936@GoTVDSB.ca>
2026-03-02 13:17:16 -0500

    close

--- Program.cs
+++ Program.cs
@@ -4,10 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Part1();
+            Part2();
         }
         public static void Part1()
         {
+
+            //assignment 1
+            for (int i = 10; i >= 0; i= i-1)
+            {
+                Console.WriteLine(i);
+               
+                
+            } 
+            Console.WriteLine("Blastoff!");
+        }
+        public static void Part2()
+        { Console.WriteLine("x\t\ty");
+            //Assignment 2
+            for (int i = -10; i <= 10; i = i + 2)
+            {
+               
+                Console.WriteLine(i + "\t\t" + (i * i) );
+            }
+        }
+        public static void Part3()
+        {
+            //assignment 3
+            string name;
+            Console.WriteLine("what is your name");
+            name= Console.ReadLine();
+
+
+
+
 
         }
     }
