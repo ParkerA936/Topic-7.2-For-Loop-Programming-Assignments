88cb7a76cf2a3f774abccfaa95924f2c15d8f7ff
Parker Arnett <362581936@GoTVDSB.ca>
2026-03-02 12:15:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Topic_7._2___For_Loop_Programming_Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+        public static void Part1()
+        {
+
+        }
+    }
+}
